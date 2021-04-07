aff1c01d7a7e4bc0a82664ee0b015997b2e2e04c
Khoa Dang <khoa.dang@hamk.fi>
2021-04-07 10:26:40 +0300

    added beeap20.py

---
+++ beeap20.py
@@ -0,0 +1,6 @@
+# todo: rename the writeYourThought function to yourname_writeYourThought
+# print whatever you want to say to the rest of your peers
+# no nsfw!
+def writeYourThought():
+    print("your Thoughts")
+    
