3a5db00c2f5c69157650799251128ce969cedeb1
mohsen20100 <82084098+mohsen20100@users.noreply.github.com>
2021-04-07 16:27:07 +0300

    Delete beeap20.py

--- beeap20.py
+++
@@ -1,6 +0,0 @@
-# todo: rename the writeYourThought function to yourname_writeYourThought
-# print whatever you want to say to the rest of your peers
-# no nsfw!
-def writeYourThought():
-    print("your Thoughts")
-    
